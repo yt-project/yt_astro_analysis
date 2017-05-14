cefb9a411de0685b17402d9ec4283bd68c21244e
Britton Smith <brittonsmith@gmail.com>
2017-05-14 09:02:37 -0700

    Updating flake8 test.

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -1,5 +1,5 @@
 import subprocess
-import yt
+import yt_astro_analysis
 import os
 
 from yt.testing import requires_module
@@ -7,15 +7,15 @@ from yt.testing import requires_module
 
 @requires_module('flake8')
 def test_flake8():
-    yt_dir = os.path.dirname(os.path.abspath(yt.__file__))
+    ytaa_dir = os.path.dirname(os.path.abspath(yt_astro_analysis.__file__))
     output_file = os.environ.get("WORKSPACE", None) or os.getcwd()
     output_file = os.path.join(output_file, 'flake8.out')
     if os.path.exists(output_file):
         os.remove(output_file)
     output_string = "--output-file=%s" % output_file
-    config_string = "--config=%s" % os.path.join(os.path.dirname(yt_dir), 
+    config_string = "--config=%s" % os.path.join(os.path.dirname(ytaa_dir),
                                                  'setup.cfg')
-    subprocess.call(['flake8', output_string, config_string, yt_dir])
+    subprocess.call(['flake8', output_string, config_string, ytaa_dir])
     
     with open(output_file) as f:
         flake8_output = f.readlines()
