5c6289785320b5394738cdc8981213b66fd11174
Britton Smith <brittonsmith@gmail.com>
2017-05-14 08:11:27 -0700

    Adding all modules to main import as a dummy import.

---
+++ yt_astro_analysis/__init__.py
@@ -0,0 +1,22 @@
+"""
+API for yt astro analysis.
+
+
+
+"""
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2017, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from yt_astro_analysis import \
+    cosmological_observation, \
+    halo_analysis, \
+    halo_finding, \
+    ppv_cube, \
+    radmc3d_export, \
+    sunyaev_zeldovich
