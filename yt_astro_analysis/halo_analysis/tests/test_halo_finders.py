9e54fcebb48eaedee6bf518b2c661b111b4392c4
Britton Smith <brittonsmith@gmail.com>
2017-08-07 17:30:16 -0700

    Make test into TempDirTest.

--- yt_astro_analysis/halo_analysis/tests/test_halo_finders.py
+++ yt_astro_analysis/halo_analysis/tests/test_halo_finders.py
@@ -8,6 +8,9 @@ from yt.utilities.answer_testing.framework import \
     FieldValuesTest, \
     requires_ds
 
+from yt_astro_analysis.utilities.testing import \
+    TempDirTest
+
 _fields = (("halos", "particle_position_x"),
            ("halos", "particle_position_y"),
            ("halos", "particle_position_z"),
@@ -17,22 +20,31 @@ methods = {"fof": 2, "hop": 2, "rockstar": 3}
 decimals = {"fof": 10, "hop": 10, "rockstar": 1}
 
 etiny = "enzo_tiny_cosmology/DD0046/DD0046"
-@requires_ds(etiny, big_data=True)
-def test_halo_finders():
-    from mpi4py import MPI
-    filename = os.path.join(os.path.dirname(__file__),
-                            "run_halo_finder.py")
-    for method in methods:
-        comm = MPI.COMM_SELF.Spawn(sys.executable,
-                                   args=[filename, method],
-                                   maxprocs=methods[method])
-        comm.Disconnect()
 
-        fn = os.path.join(os.path.dirname(__file__),
-                          "halo_catalogs", method,
-                          "%s.0.h5" % method)
-        ds = load(fn)
-        assert isinstance(ds, HaloCatalogDataset)
-        for field in _fields:
-            yield FieldValuesTest(ds, field, particle_type=True,
-                                  decimals=decimals[method])
+class RockstarTest(TempDirTest):
+
+    def setUp(self):
+        try:
+            from mpi4py import MPI
+        except:
+            self.skipTest("cannot import MPI")
+        super(RockstarTest, self).setUp()
+
+    @requires_ds(etiny, big_data=True)
+    def test_halo_analysis_finders(self):
+        filename = os.path.join(os.path.dirname(__file__),
+                                "run_halo_finder.py")
+        for method in methods:
+            comm = MPI.COMM_SELF.Spawn(sys.executable,
+                                       args=[filename, method],
+                                       maxprocs=methods[method])
+            comm.Disconnect()
+
+            fn = os.path.join(os.path.dirname(__file__),
+                              "halo_catalogs", method,
+                              "%s.0.h5" % method)
+            ds = load(fn)
+            assert isinstance(ds, HaloCatalogDataset)
+            for field in _fields:
+                yield FieldValuesTest(ds, field, particle_type=True,
+                                      decimals=decimals[method])
