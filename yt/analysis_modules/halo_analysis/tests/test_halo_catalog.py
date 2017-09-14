8749c7992d1818542c8eab80f7b026683a59304a
Britton Smith <brittonsmith@gmail.com>
2017-09-14 10:42:10 -0700

    Adding halo quantity test.

--- yt/analysis_modules/halo_analysis/tests/test_halo_catalog.py
+++ yt/analysis_modules/halo_analysis/tests/test_halo_catalog.py
@@ -39,27 +39,45 @@ class HaloQuantityTest(AnswerTestingTest):
     _type_name = "HaloQuantity"
     _attrs = ()
 
+<<<<<<< HEAD
     def __init__(self, data_ds_fn, halos_ds_fn):
         self.data_ds_fn = data_ds_fn
         self.halos_ds_fn = halos_ds_fn
         self.ds = data_dir_load(data_ds_fn)
+=======
+    def __init__(self, data_ds, halos_ds):
+        self.data_ds = data_dir_load(data_ds)
+        self.halos_ds = data_dir_load(halos_ds)
+        self.ds = data_ds
+>>>>>>> d04a35d0a... Adding halo quantity test.
 
     def run(self):
         curdir = os.getcwd()
         tmpdir = tempfile.mkdtemp()
         os.chdir(tmpdir)
 
+<<<<<<< HEAD
         dds = data_dir_load(self.data_ds_fn)
         hds = data_dir_load(self.halos_ds_fn)
         hc = HaloCatalog(
             data_ds=dds, halos_ds=hds,
             output_dir=os.path.join(tmpdir, str(dds)))
+=======
+        hc = HaloCatalog(
+            data_ds=self.data_ds, halos_ds=self.halos_ds,
+            output_dir=os.path.join(tmpdir, str(self.data_ds)))
+>>>>>>> d04a35d0a... Adding halo quantity test.
         hc.add_callback("sphere")
         hc.add_quantity("nstars")
         hc.create()
 
+<<<<<<< HEAD
         fn = os.path.join(tmpdir, str(dds),
                           "%s.0.h5" % str(dds))
+=======
+        fn = os.path.join(tmpdir, str(self.data_ds),
+                          "%s.0.h5" % str(self.data_ds))
+>>>>>>> d04a35d0a... Adding halo quantity test.
         ds = load(fn)
         ad = ds.all_data()
         mi, ma = ad.quantities.extrema("nstars")
@@ -80,4 +98,10 @@ e64 = "Enzo_64/DD0043/data0043"
 @requires_ds(rh0)
 @requires_ds(e64)
 def test_halo_quantity():
+<<<<<<< HEAD
     yield HaloQuantityTest(e64, rh0)
+=======
+    hds = data_dir_load(rh0)
+    dds = data_dir_load(e64)
+    yield HaloQuantityTest(dds, hds)
+>>>>>>> d04a35d0a... Adding halo quantity test.
