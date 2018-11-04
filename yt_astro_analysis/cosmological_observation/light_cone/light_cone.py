466781e8b98de97bb1ec0d126ec145e8779d3cee
Britton Smith <brittonsmith@gmail.com>
2018-11-04 12:53:52 -0800

    Allow light cone tests to test different fields and add new test.

--- yt_astro_analysis/cosmological_observation/light_cone/light_cone.py
+++ yt_astro_analysis/cosmological_observation/light_cone/light_cone.py
@@ -335,7 +335,7 @@ class LightCone(CosmologySplice):
                 pixel_area = (proper_box_size.in_cgs() / pixels)**2 #in proper cm^2
                 factor = pixel_area / (4.0 * np.pi * dL.in_cgs()**2)
                 mylog.info("Distance to slice = %s" % dL)
-                frb['field'] *= factor #in erg/s/cm^2/Hz on observer"s image plane.
+                frb['field'] *= factor #in erg/s/cm^2/Hz on observer's image plane.
 
             my_storage.result = frb
 
