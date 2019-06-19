082a3508ec49c7f2bdc4b2a9db49aaba5b03edad
John ZuHone <jzuhone@gmail.com>
2019-06-18 22:06:25 -0400

    yt-4.0 related fixes

--- yt_astro_analysis/ppv_cube/ppv_cube.py
+++ yt_astro_analysis/ppv_cube/ppv_cube.py
@@ -308,9 +308,10 @@ class PPVCube(object):
         w.wcs.cunit = [units,units,vunit]
         w.wcs.ctype = ["LINEAR","LINEAR",vtype]
 
-        fib = FITSImageData(self.data.transpose(), fields=self.field, wcs=w)
-        fib.update_all_headers("bunit", re.sub('()', '', str(self.proj_units)))
-        fib.update_all_headers("btype", self.field)
+        fib = FITSImageData(self.data, fields=self.field, wcs=w)
+        fib.update_header("all", "wcsname", "spectral_cube")
+        fib.update_header("all", "bunit", re.sub('()', '', str(self.proj_units)))
+        fib.update_header("all", "btype", self.field)
         if sky_scale is not None and sky_center is not None:
             fib.create_sky_wcs(sky_center, sky_scale, **kwargs)
         fib.writeto(filename, overwrite=overwrite)
