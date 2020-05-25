f09ef98a8e0907542569d199b7ca2b6c4be0cb6b
Britton Smith <brittonsmith@gmail.com>
2020-05-25 14:13:45 +0100

    Move imports to __init__.

---
+++ yt_astro_analysis/halo_analysis/__init__.py
@@ -0,0 +1,17 @@
+from yt_astro_analysis.halo_analysis.halo_catalog.halo_catalog import \
+    HaloCatalog
+
+from yt_astro_analysis.halo_analysis.halo_catalog.halo_callbacks import \
+    add_callback
+
+from yt_astro_analysis.halo_analysis.halo_catalog.halo_finding_methods import \
+    add_finding_method
+
+from yt_astro_analysis.halo_analysis.halo_catalog.halo_filters import \
+    add_filter
+
+from yt_astro_analysis.halo_analysis.halo_catalog.halo_quantities import \
+    add_quantity
+
+from yt_astro_analysis.halo_analysis.halo_catalog.halo_recipes import \
+    add_recipe
